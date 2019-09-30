fc634f6c4d8e6c5a807edbb3f072958feed05b9d
Ignas Leonavicius <ignas777@gmail.com>
2019-09-30 12:43:16 +0300

    completed js exercises

--- sass/js/main.js
+++ sass/js/main.js
@@ -16,7 +16,9 @@ var email = "ignas777@gmail.com";
 var city = "Kaunas";
 var line = "Mano vardas yra " + name + ", emailas - " + email + ". " + city + " yra miestas kuriame as gyvenu.";
 console.log(line);
-console.log(line.length);
-console.log(line.toLowerCase());
 
-console.log(line[30]);
+console.log(line.toLowerCase());
+console.log(line.length);
+console.log(line[35]);
+console.log(line.indexOf("las"));
+console.log(line.slice(60, 80));
