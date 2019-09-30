10d41f8fe7e12172f376087318810c801152dca0
tomasa90 <tomasa90@gmail.com>
2019-09-30 11:36:32 +0300

    added js to sass

---
+++ sass/js/main.js
@@ -0,0 +1,22 @@
+var vardas = "Ignas";
+let nameInLet;
+
+console.log(typeof vardas);
+console.log(typeof true);
+console.log(typeof 211546464);
+console.log(typeof ['pienas']);
+console.log(typeof {vardas: 'Ignas'});
+const margins = "120px";
+console.log(margins);
+console.log(vardas + " " + margins);
+
+
+var name = "Ignas";
+var email = "ignas777@gmail.com";
+var city = "Kaunas";
+var line = "Mano vardas yra " + name + ", emailas - " + email + ". " + city + " yra miestas kuriame as gyvenu.";
+console.log(line);
+console.log(line.length);
+console.log(line.toLowerCase());
+
+console.log(line[30]);
