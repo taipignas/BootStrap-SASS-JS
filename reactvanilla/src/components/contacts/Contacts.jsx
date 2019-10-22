7fd683cd9c8c851e1df3a97def3e235ac14fee25
Ignas Leonavicius <ignas777@gmail.com>
2019-10-22 18:39:01 +0300

    added react form to contacts

--- reactvanilla/src/components/contacts/Contacts.jsx
+++ reactvanilla/src/components/contacts/Contacts.jsx
@@ -1,15 +1,48 @@
-import React from 'react';
+import React,{ Component } from 'react';
 import styles from './contacts.scss';
-function Contacts() {
-    return (
-        <div className="col">
-            <h3>Kontaktai</h3>
-            <ul >
-                <li className='d-inline-block px-3'>firma1</li>
-                <li className='d-inline-block px-3'>firma2</li>
-            </ul>
-        </div>
-    );
+class Contacts extends Component {
+   constructor(){
+       super()
+       this.state = {
+           vardas: '',
+           pavarde: '',
+           elPastas: '',
+           zinute: '',
+       }
+   }
+   handleFormSubmit = e => {
+       e.preventDefault();
+       alert(this.state);
+   }
+   render(){
+       return (
+           <section>
+               <h2>Kontaktai</h2>
+               <form action="#" >
+                   <label>Vardas</label>
+                   <input type="text" id="fname" name="firstname" placeholder="Vardas"
+                          value={this.state.vardas}
+                          onChange={e => this.setState({ vardas: e.target.value })}
+                   />
+                   <label>Pavardė</label>
+                   <input type=" text" id="lname" name="pavarde" placeholder="Pavarde"
+                          value={this.state.pavarde}
+                          onChange={e => this.setState({ pavarde: e.target.value })}
+                   />
+                   <label>El. paštas</label>
+                   <input type="email" id="email" name="elpastas" placeholder="El. paštas"
+                          value={this.state.elPastas}
+                          onChange={e => this.setState({ elPastas: e.target.value })}
+                   />
+                   <label>Žinutė</label>
+                   <textarea id="message" name="message" placeholder="Jusu zinute.."
+                             onChange={e => this.setState({ zinute: e.target.value })}
+                             value={this.state.zinute}
+                   ></textarea>
+                   <input type="submit" onClick={e => this.handleFormSubmit(e)} value="Siųsti" />
+               </form >
+           </section>
+       );
+   }
 }
-
 export default Contacts;
